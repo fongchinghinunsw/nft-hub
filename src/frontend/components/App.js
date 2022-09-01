9b63116bd5a63a94215f7cf68ef237e48bfa97d4
Stephen Fong <fongchinghinunsw@gmail.com>
2022-09-01 16:57:09 +0800

    Add backend contracts and tests

---
+++ src/frontend/components/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 ms-3"
+          href="http://www.koala-techs.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nft Hub
+        </a>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center"></main>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
